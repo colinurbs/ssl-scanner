fd1e2177be587eb449c3f10569016fd89a8afbc3
Colin Urban <colin.urban@gmail.com>
2016-11-19 18:19:30 -0500

    datatables

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $sites = array();
-echo "Reading CSV <br>";
+
 if (($handle = fopen("Inventory.csv", "r")) !== FALSE) {
 	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 		$num = count($data);
@@ -12,29 +12,58 @@ if (($handle = fopen("Inventory.csv", "r")) !== FALSE) {
 		
 	}
 	fclose($handle);
-}
-echo "Testing Sites <br>";
-foreach ($sites as $url)
-{
-	echo "<b>".$url ."</b><br>";
-	$orignal_parse = parse_url("https://".$url, PHP_URL_HOST);
-	$get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
-	$read = stream_socket_client("ssl://".$orignal_parse.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
-	$cert = stream_context_get_params($read);
-	$certinfo = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
+}?>
+<h3>Sites</h3>
+<table class="table" id="sites">
+	<thead>
+		<tr>
+			<th>Site</th>
+			<th>Issuer</th>
+			<th>Type</th>
+			<th>Expiration</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		foreach ($sites as $url)
+		{
+			$orignal_parse = parse_url("https://".$url, PHP_URL_HOST);
+			$get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
+			$read = stream_socket_client("ssl://".$orignal_parse.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
+			$cert = stream_context_get_params($read);
+			$certinfo = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
 
-	if(isset($certinfo['issuer']['CN']))
-	{
-		echo $certinfo['issuer']['CN'].'<br>';
-		echo $certinfo['signatureTypeSN'].'<br>';
+			if(isset($certinfo['issuer']['CN']))
+			{
+				echo "<tr><td>";
+				echo $url;
+				echo "</td><td>";
+				echo $certinfo['issuer']['CN'];
+				echo "</td><td>";
+				echo $certinfo['signatureTypeSN'];
+				echo "</td><td>";
+				echo date("Y-m-d", $certinfo['validTo_time_t']);
+				echo "</td></tr>";
+			}
+			else
+			{
+				
+			}
+		}
+		?>
+	</tbody>
+</table>
+<script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+<script>
+	$(document).ready(function() {
+		 
+		 $('#sites').DataTable();
+		 
+	});
 
-	 // echo date("Y-m-d", $certinfo['validFrom_time_t']).'<br>';
-
-		echo "Expires: " . date("Y-m-d", $certinfo['validTo_time_t']).'<br>';
-	}
-	else
-	{
-		echo "SSL not found<br>";
-	}
-}
+</script>
 
